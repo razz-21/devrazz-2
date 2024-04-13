68ce1f76afd6a0cd6fcdc942d224d209a9a2c1ea
razz.ernesto@gmail.com <ernesto@muuvment.com>
2024-04-13 22:36:07 +0800

    refactor: replace CV

--- src/lib/HeadContent.svelte
+++ src/lib/HeadContent.svelte
@@ -117,7 +117,7 @@
         <div style:position={"relative"} style:overflow={"hidden"}>
           <Saos animation={`1s ease ${ screenType === "desktop" ? '3.5s' : '0s' } slide-overflow-up both`} once={true}>
             <a id="getCvBtn" type="button" class="btn btn-primary-gradient" style="margin: 3rem 0; display: inline-block;"
-              href="/CV - Ernesto Razo [02.04.2023].pdf" download="CV - Ernesto Razo">
+              href="/CV - Ernesto Razo [04.13.2024].pdf" download="CV - Ernesto Razo">
               Get CV
             </a>
           </Saos>
